2a83aa0b11a697b7cc5145a9b182bd22bd72d9e4
Andy Carter <andyjcarter77@gmail.com>
2024-01-05 08:03:37 +0000

    fix pub name summary

--- app/static/javascript/pub/populate_summary.js
+++ app/static/javascript/pub/populate_summary.js
@@ -4,6 +4,6 @@ function populate_summary(id) {
         return x.pub_identity == id
     });
     text_ref = summary_pub[0]['detail_name'].toString().substring(0,24)
-    document.getElementById('summary_name').innerHTML = "<a style='font-family: Verdana, sans-serif; text-align: center; text-decoration: none; font-size: 12px; font-weight: bold; color: white;'>" + text_ref + "</a>"
+    document.getElementById('summary_name').innerHTML = "<a style='font-family: Verdana, sans-serif; text-align: center; text-decoration: none; font-size: 12px; font-weight: bold;'>" + text_ref + "</a>"
     document.getElementById('summary_extra').innerHTML = "<a>" + summary_pub[0]['extra'] + "</a>"
 }
