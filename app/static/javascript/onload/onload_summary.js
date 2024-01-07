3d7e5f8bbe9cc372e43e593e7030094baeda4ead
Andy Carter <andyjcarter77@gmail.com>
2024-01-07 14:44:41 +0000

    remove photos add map

--- app/static/javascript/onload/onload_summary.js
+++ app/static/javascript/onload/onload_summary.js
@@ -2,9 +2,9 @@ function onload_summary() {
     console.log('onload summary')
 
     populate_summary(daily_id)
-    populate_photo()
+    //populate_photo()
     populate_autocomplete()
-    //map_load_summary()
+    map_load_summary()
     add_listeners()
     //onload_collection()
 
