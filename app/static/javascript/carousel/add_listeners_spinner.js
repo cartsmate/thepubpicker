3e363fd7ba3d148ee41583af8bceaacc6c0a2079
Andy Carter <andyjcarter77@gmail.com>
2024-01-12 15:19:39 +0000

    on off spinner

--- app/static/javascript/carousel/add_listeners_spinner.js
+++ app/static/javascript/carousel/add_listeners_spinner.js
@@ -7,90 +7,72 @@ function add_listeners_spinner() {
     for (field in model_format) {
         var marker = model_format[field]
         if (marker != 'nofeature') {
-            var marker = model_format[field] + "_carousel"
-            console.log('marker named: ' + marker)
-            document.getElementById(marker).addEventListener('click', (function (marker) {
+            var marker_id = model_format[field] + "_carousel"
+            document.getElementById(marker_id).addEventListener('click', (function (marker) {
                 return function () {
                     console.log('carousel spinner listener triggered')
-
-                    underscore = marker.indexOf("_");
-                    attr_marker = marker.substr(0,underscore);
+                    //console.log(marker)
+                    //underscore = marker.indexOf("_");
+                    //attr_marker = marker.substr(0,underscore);
                     //console.log(attr_marker)
-                    for (i=0; i<model_formats['icon_list'].length; i++) {
-                        console.log(model_formats['icon_list'][i])
-                        if (model_formats['icon_list'][i] != 'nofeature') {
-                            document.getElementById(model_formats['icon_list'][i] + "_filter").checked = false;
-                            document.getElementById(model_formats['icon_list'][i] + "_word").style.color = "black"
-                            document.getElementById(model_formats['icon_list'][i] + "_carousel").style.backgroundColor = "white"
-                        }
-                    }
-                    /*
-                    document.getElementById("brunch_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("dart_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("entertain_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("favourite_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("garden_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("history_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("late_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("music_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("pool_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("quiz_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById(marker).style.backgroundColor = "white"
-                    document.getElementById("roast_filter").checked = false;
-                    document.getElementById(attr_marker + "_word").style.color = "black"
-                    document.getElementById("sport_carousel").style.backgroundColor = "white"
-                    document.getElementById("sport_filter").checked = false;
-                    document.getElementById("sport_word").style.color = "black"
-                    document.getElementById("sport_carousel").style.backgroundColor = "white"
-                    */
-                    document.getElementById(attr_marker + "_filter").checked = true;
-                    document.getElementById(attr_marker + "_word").style.color = "white"
-                    document.getElementById(marker).style.backgroundColor = "coral"
-
-                    pubs_reviews = filter_all_data()
-                    console.log('pubs_reviews: ' + pubs_reviews.length)
-                    pubs_sorted = sort_by_distance(pubs_reviews, map)
-                    var center = new google.maps.LatLng(pubs_sorted[0]['detail_latitude'], pubs_sorted[0]['detail_longitude']);
-                    console.log('center: ' + center)
-                    map.panTo(center);
-//                    populate_header(pubs_sorted.length)
-//                    map_load_collection(pubs_sorted)
-//                    list_setup(pubs_sorted)
-                    document.getElementById('template_map').style.display = "block";
-                    document.getElementById('template_list').style.display = "block";
-                    document.getElementById('template_header').style.display = "block";
-//                    var base_url = window.location.hostname
-//                    if (env_vars['env'] == 'qual') {
-//                        var url = "http://" + base_url + ":5000/collection/"
-//                    } else {
-//                        var url = "http://" + base_url + "/collection/"
+//                    for (i=0; i<model_formats['icon_list'].length; i++) {
+//                        //console.log(model_formats['icon_list'][i])
+//                        if (model_formats['icon_list'][i] != 'nofeature') {
+//                            //console.log(model_formats['icon_list'][i])
+//                            document.getElementById(model_formats['icon_list'][i] + "_filter").checked = false;
+//                            document.getElementById(model_formats['icon_list'][i] + "_word").style.color = "black"
+//                            document.getElementById(model_formats['icon_list'][i] + "_carousel").style.backgroundColor = "white"
+//                        }
 //                    }
-//                    const myUrlWithParams = new URL(url);
-//                    underscore = marker.indexOf("_");
-//                    attr_marker = marker.substr(0,underscore);
-//                    myUrlWithParams.searchParams.append('feature', attr_marker);
-//                    window.location.replace(myUrlWithParams.href);
+
+                    if (document.getElementById(marker + "_filter").checked == true){
+                        console.log('already TRUE - blank out')
+                        for (i=0; i<model_formats['icon_list'].length; i++) {
+                        //console.log(model_formats['icon_list'][i])
+                            if (model_formats['icon_list'][i] != 'nofeature') {
+                                //console.log(model_formats['icon_list'][i])
+                                document.getElementById(model_formats['icon_list'][i] + "_filter").checked = false;
+                                document.getElementById(model_formats['icon_list'][i] + "_word").style.color = "black"
+                                document.getElementById(model_formats['icon_list'][i] + "_carousel").style.backgroundColor = "white"
+                            }
+                        }
+                        //document.getElementById(marker + "_filter").checked = false;
+                        //document.getElementById(marker + "_word").style.color = "black"
+                        //document.getElementById(marker + "_carousel").style.backgroundColor = "white"
+                        document.getElementById('template_map').style.display = "none";
+                        document.getElementById('template_list').style.display = "none";
+                        document.getElementById('template_header').style.display = "none";
+                    } else {
+                        console.log(marker)
+                        console.log('was blank - make HIGHLIGHT')
+                        for (i=0; i<model_formats['icon_list'].length; i++) {
+                        //console.log(model_formats['icon_list'][i])
+                            if (model_formats['icon_list'][i] != 'nofeature') {
+                                //console.log(model_formats['icon_list'][i])
+                                document.getElementById(model_formats['icon_list'][i] + "_filter").checked = false;
+                                document.getElementById(model_formats['icon_list'][i] + "_word").style.color = "black"
+                                document.getElementById(model_formats['icon_list'][i] + "_carousel").style.backgroundColor = "white"
+                            }
+                        }
+                        document.getElementById(marker + "_filter").checked = true;
+                        document.getElementById(marker + "_word").style.color = "white"
+                        document.getElementById(marker + "_carousel").style.backgroundColor = "coral"
+
+                        pubs_reviews = filter_all_data()
+                        console.log('pubs_reviews: ' + pubs_reviews.length)
+                        pubs_sorted = sort_by_distance(pubs_reviews, map)
+                        var center = new google.maps.LatLng(pubs_sorted[0]['detail_latitude'], pubs_sorted[0]['detail_longitude']);
+                        console.log('center: ' + center)
+                        map.panTo(center);
+    //                    populate_header(pubs_sorted.length)
+    //                    map_load_collection(pubs_sorted)
+    //                    list_setup(pubs_sorted)
+                        document.getElementById('template_map').style.display = "block";
+                        document.getElementById('template_list').style.display = "block";
+                        document.getElementById('template_header').style.display = "block";
                     }
-                })(marker))
+                }
+            })(marker))
         }
     }
 }
