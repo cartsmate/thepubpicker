55f85635f5f4173d5c1f9c6074e7aca6cda8e41b
Andy Carter <andyjcarter77@gmail.com>
2024-01-12 19:30:50 +0000

    swipe carousel

--- app/static/javascript/carousel/spinner.js
+++ app/static/javascript/carousel/spinner.js
@@ -6,31 +6,18 @@ const carousel = document.querySelector('._carousel');
 const btnFFWD = document.querySelector('#forwards');
 const btnRWD = document.querySelector('#rewards');
 
-/**create variables for faces
- * and one for the degree rate
- * to organize the faces in an
- * symmetric order
- */
 let faceCount = 12;
 let stepCount = 360 / faceCount;
-
 let count = 0;
 
-
 btnFFWD.addEventListener('click',()=>{
-
     let stepForward = count += stepCount;
-
     nextStep(stepForward);
-
 });
 
 btnRWD.addEventListener('click',()=>{
-
     let stepReward = count -= stepCount;
-
     nextStep(stepReward);
-
 });
 
 
@@ -39,18 +26,11 @@ btnRWD.addEventListener('click',()=>{
 * jumps over/under zero
 */
 function nextStep(x){
-
     if(x <= 0){
-
         x *= -1; //turn to positive number again
-
         carousel.style.transform = `rotateY(${x}deg)`;
-
-    }
-    else if( x >= 0){
-
+    } else if( x >= 0){
         carousel.style.transform = `rotateY(-${x}deg)`;
-
     }
 }
 
---
+++ app/static/javascript/carousel/spinner_swipe.js
@@ -0,0 +1,37 @@
+
+
+(function(){
+    var $slider = $('#the_carousel');
+    const carousel = document.querySelector('._carousel');
+    const btnFFWD = document.querySelector('#forwards');
+    const btnRWD = document.querySelector('#rewards');
+
+    let faceCount = 12;
+    let stepCount = 360 / faceCount;
+    let count = 0;
+
+   $slider.on( "swipeleft", function(){ // show next image on swipe left
+        let stepForward = count += stepCount;
+        nextStep(stepForward);
+    }).on( "swiperight", function(){ // show prev image on swipe right
+        let stepReward = count -= stepCount;
+        nextStep(stepReward);
+    });
+
+
+
+/** ▼ this function covers
+* the cases when counter
+* jumps over/under zero
+*/
+    function nextStep(x){
+        if(x <= 0){
+            x *= -1; //turn to positive number again
+            carousel.style.transform = `rotateY(${x}deg)`;
+        } else if( x >= 0){
+            carousel.style.transform = `rotateY(-${x}deg)`;
+        }
+    }
+
+
+}());
