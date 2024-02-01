9bfe9fd8082f148e0afec1253f0738e3698d1533
Andy Carter <andyjcarter77@gmail.com>
2024-02-01 22:11:40 +0000

    version 17

--- app/static/javascript/pub/populate_photo_carousel.js
+++ app/static/javascript/pub/populate_photo_carousel.js
@@ -16,6 +16,7 @@ function populate_photo_carousel() {
         */
         api_call_text = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=' + photos_list[i] + '&key=' + env_vars['places_key']
         document.getElementById("photo_" + i).src = api_call_text
+        //document.getElementById("photo_" + i).alt = photos_list[i]
 
         //div_element.appendChild(img_element)
 
