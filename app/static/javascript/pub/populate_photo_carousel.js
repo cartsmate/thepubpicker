17864a59e0c662b36bffdd2393e0a2db6be120fe
Andy Carter <andyjcarter77@gmail.com>
2024-02-02 01:31:25 +0000

    removing preload

--- app/static/javascript/pub/populate_photo_carousel.js
+++ app/static/javascript/pub/populate_photo_carousel.js
@@ -2,8 +2,7 @@ function populate_photo_carousel() {
     console.log('populate photo carousel')
     //api_call_text = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=' + photos_list[0] + '&key=' + env_vars['places_key']
     //document.getElementById('photo_0').src = api_call_text
-    console.log('images')
-    console.log(images)
+
 
     //for (i = 0; i < photos_list.length; i++) {
     for (i = 0; i < photos_list.length; i++) {
