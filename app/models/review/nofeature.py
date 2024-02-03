300bbdf7743eada25c5b0675e0cbfa773e8ea293
Andy Carter <andyjcarter77@gmail.com>
2024-02-03 00:56:00 +0000

    more pubs

--- app/models/review/nofeature.py
+++ app/models/review/nofeature.py
@@ -3,7 +3,7 @@
 class NoFeature:
     def __init__(self, name="nofeature", alias="None", alias2="None", required=True, form_visible=False,
                  table_visible=False, value='1', control="check", icon="nofeature.png", data_type='str',
-                 menu_filter=False, quick_filter=False, event_filter=False):
+                 menu_filter=True, quick_filter=False, event_filter=False):
         self.name = name
         self.alias = alias
         self.alias2 = alias2
