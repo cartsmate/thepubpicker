4bfcd4b1ce949177f8ffe7f6eae9bc607479fa5d
Andy Carter <andyjcarter77@gmail.com>
2024-03-01 12:23:39 +0000

    reinstate photos

--- app/static/javascript/pub/populate_photo_carousel.js
+++ app/static/javascript/pub/populate_photo_carousel.js
@@ -1,8 +1,8 @@
 function populate_photo_carousel() {
-    /*
+
     for (i = 0; i < photos_list.length; i++) {
         api_call_text = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=' + photos_list[i] + '&key=' + env_vars['places_key']
         document.getElementById("photo_" + i).src = api_call_text
     }
-    */
+
 }
