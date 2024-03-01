d6b60ec00a13e7ab7819f9f5d8b9fdf02c8b865d
Andy Carter <andyjcarter77@gmail.com>
2024-03-01 11:05:32 +0000

    bug fix

--- app/static/javascript/pub/populate_summary.js
+++ app/static/javascript/pub/populate_summary.js
@@ -3,12 +3,12 @@ function populate_summary(show_pub) {
 
     //text_ref = summary_pub[0]['detail_name'].toString().substring(0,22)  + " | " + summary_pub[0]['station_name'].toString().substring(0,18)
     text_ref = show_pub[0]['detail_name'].toString().substring(0,22)  + " | " + show_pub[0]['station_name'].toString().substring(0,18)
-    document.getElementById('summary_name').innerHTML = "<a style='text-decoration: none; justify-content: center; font-size: 14px; font-weight: bold;'>" + text_ref + "</a>"
+    document.getElementById('summary_name').innerHTML = "<div style='text-decoration: none; justify-content: center; font-size: 14px; font-weight: bold;'>" + text_ref + "</div>"
     populate_extra(show_pub)
 }
 function populate_extra(show_pub) {
     console.log('populate_extra')
-    document.getElementById('summary_extra').innerHTML = "<a>" + show_pub[0]['extra'] + "</a>"
+    document.getElementById('summary_extra').innerHTML = "<div>" + show_pub[0]['extra'] + "</div>"
 }
 function populate_title(show_pub) {
     console.log('populate_title')
