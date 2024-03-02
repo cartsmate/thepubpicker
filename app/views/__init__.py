3cbd1936722c8ec6f47a98fc39c6182f6bc374db
Andy Carter <andyjcarter77@gmail.com>
2024-03-02 10:10:20 +0000

    updated version

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -7,13 +7,16 @@ from .pp_06_submit import submit
 from app import app
 from flask import render_template, request
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
 
 @app.errorhandler(403)
 def page_forbidden(e):
