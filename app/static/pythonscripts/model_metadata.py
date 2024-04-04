57a0f4f87bdcc826c5f6761544df6c5dfed4cd9d
Andy Carter <andycarter@Andys-MacBook-Pro.local>
2024-04-04 16:03:05 +0100

    classed out pub router

--- app/static/pythonscripts/model_metadata.py
+++ app/static/pythonscripts/model_metadata.py
@@ -1,17 +1,30 @@
 import json
 
+from app.models.daily_event.daily_event import DailyEvent
+from app.models.detail.detail import Detail
+from app.models.direction.direction import Direction
 from app.models.diary.diary import Diary
 from app.models.pub.pub import Pub
 from app.models.review.review import Review
+from app.models.station.station import Station
 
 
 class ModelMetadata:
     @staticmethod
     def get_model_metadata():
-        review_json = json.loads(json.dumps(Review().__dict__, default=lambda o: o.__dict__))
+        detail_json = json.loads(json.dumps(Detail().__dict__, default=lambda o: o.__dict__))
+        direction_json = json.loads(json.dumps(Direction().__dict__, default=lambda o: o.__dict__))
         diary_json = json.loads(json.dumps(Diary().__dict__, default=lambda o: o.__dict__))
+        event_json = json.loads(json.dumps(DailyEvent().__dict__, default=lambda o: o.__dict__))
+        review_json = json.loads(json.dumps(Review().__dict__, default=lambda o: o.__dict__))
         pub_obj_json = json.loads(json.dumps(Pub().__dict__, default=lambda o: o.__dict__))
-        models_json = {'review_json': review_json,
+        station_json = json.loads(json.dumps(Station().__dict__, default=lambda o: o.__dict__))
+
+        models_json = {'detail_json': detail_json,
                        'diary_json': diary_json,
-                       'pub_obj_json': pub_obj_json}
+                       'direction_json': direction_json,
+                       'event_json': event_json,
+                       'pub_obj_json': pub_obj_json,
+                       'review_json': review_json,
+                       'station_json': station_json}
         return models_json
--- app/views/pp_03_pub.py
+++ app/views/pp_03_pub.py
@@ -1,24 +1,12 @@
-import json
 from app import app
 from flask import render_template, request, session, redirect, url_for
-from app.static.pythonscripts.uuid_generater import UuidGenerator
-from app.models.detail.detail import Detail
-from app.models.review.review import Review
-from app.models.diary.diary import Diary
-from app.models.station.station import Station
-from app.models.direction.direction import Direction
-from app.models.daily_event.daily_event import DailyEvent
-from app.models.pub_record.pub_record import PubRecord
+
 from app.models.photo.photo import Photo
+
 from app.static.pythonscripts.files_pub import FilesPub
-from app.static.pythonscripts.files_photo import FilesPhoto
-from app.static.pythonscripts.files_daily import FilesDaily
-# from app.static.pythonscripts.files_events import FilesEvent
 from app.static.pythonscripts.pub_get import GetPub
-from app.static.pythonscripts.dataframes import Dataframes
-# from app.static.pythonscripts.controls_list import ControlsList
-# from app.static.pythonscripts.objects import Objects
-from app.static.pythonscripts.multi_threading import MultiThreadingPub
+from app.static.pythonscripts.model_metadata import ModelMetadata
+from app.static.pythonscripts.source_data import SourceData
 from config import *
 
 
@@ -29,117 +17,47 @@ def get_csv_data(model):
 
 @app.route("/pub/", methods=['GET', 'POST'])
 def pub():
-    # print('session')
-    # print(session)
-    # print("Configurations().get_config2()['session_key']")
-    # print(Configurations().get_config2()['session_key'])
-
+    # # # GET ENVIRONMENTAL VARIABLES
     env_vars = Configurations.get_config()
 
+    # CONFIRM THAT PAGE IS A REDIRECT FROM HOME
     if env_vars['session_key'] in session.values():
-        print('included')
-
         print('START pub')
 
         filters = request.args.get('filters')
 
-        # # # GET ENVIRONMENTAL VARIABLES
-        # env_vars = Configurations().get_config2()
-
         # # # GET REQUESTED PUB
         pub_id = request.args.get('id')
 
-        if env_vars['source'] == 'db':
-            df_dict = MultiThreadingPub().thread_caller()
-            # DATABASE FILES
-            df_daily_event_all = df_dict['df_daily_event']
-            df_station_all = df_dict['df_station']
-            df_direction_all = df_dict['df_direction']
-            df_pub_record_all = df_dict['df_pub_record']
-            # df_detail_all = df_dict['df_detail']
-            # df_review_all = df_dict['df_review']
-            # df_daily_event_all = df_dict['df_daily_event']
-            # df_diary_all = df_dict['df_diary']
-            # df_station_all = df_dict['df_station']
-            # df_direction_all = df_dict['df_direction']
-            df_pub = df_pub_record_all
-            photos_list = FilesPhoto().go_get_1_photo_request(df_pub_record_all, pub_id, env_vars)
-            stations_directions_list = Dataframes().go_get_stations_directions_list_flat(df_pub_record_all,
-                                                                                         df_station_all,
-                                                                                         df_direction_all)
-            directions_list = Dataframes().go_get_directions_list_flat(df_pub_record_all, df_station_all,
-                                                                       df_direction_all)
-        elif env_vars['source'] == 'new_csv':
-            df_daily_event_all = get_csv_data(DailyEvent())
-            df_station_all = get_csv_data(Station())
-            df_direction_all = get_csv_data(Direction())
-            df_pub_record_all = get_csv_data(PubRecord())
-
-            stations_directions_list = Dataframes().go_get_stations_directions_list_flat(df_pub_record_all,
-                                                                                         df_station_all,
-                                                                                         df_direction_all)
-            directions_list = Dataframes().go_get_directions_list_flat(df_pub_record_all, df_station_all,
-                                                                       df_direction_all)
-            # # # GET ALL DATA # # #
-            df_pub = df_pub_record_all
-            # # # GET FEATURED PUB # # #
-            daily_id = FilesDaily().go_get_details_daily(df_pub_record_all)
-            # # # GET FEATURED PUB PHOTOS # # #
-            photos_list = FilesPhoto().go_get_1_photo_request(df_pub_record_all, daily_id, env_vars)
-        else:
-            # GET DATA FROM CSV
-            df_detail_all = get_csv_data(Detail)
-            df_review_all = get_csv_data(Review())
-            df_daily_event_all = get_csv_data(DailyEvent())
-            df_diary_all = get_csv_data(Diary())
-            df_station_all = get_csv_data(Station())
-            df_direction_all = get_csv_data(Direction())
-
-            df_pub = FilesPub().get_pub_all(df_detail_all, df_review_all, df_diary_all, df_station_all, df_direction_all)
-            photos_list = FilesPhoto().go_get_1_photo_request(df_detail_all, pub_id, env_vars)
-
-            stations_directions_list = Dataframes().go_get_stations_directions_list(df_detail_all, df_station_all,
-                                                                                    df_direction_all)
-            directions_list = Dataframes().go_get_directions_list(df_detail_all, df_station_all, df_direction_all)
-
-        df_1_pub = FilesPub().get_pub_1(df_pub, pub_id)
-        pub_json = df_1_pub.to_dict(orient='records')
-
-        # CSV FILES
-        # df_1_event = GetPub().get_1(DailyEvent(), pub_id)
-        df_1_event = df_daily_event_all.loc[df_daily_event_all['pub_identity'] == pub_id]
-        df_1_event_list_json = df_1_event.to_json(orient='records')
+        # GET DATA FROM SOURCE (DETERMINED BY CONFIG)
+        source_data = SourceData().get_source_data(env_vars)
+        pub_json = FilesPub().get_pub_1(source_data['df_pub'], pub_id).to_dict(orient='records')
+        df_1_event_list_json = source_data['df_daily_event_all'].loc[source_data['df_daily_event_all']['pub_identity'] == pub_id].to_json(orient='records')
         json_loads = json.loads(df_1_event_list_json)
 
-        detail_json = json.loads(json.dumps(Detail().__dict__, default=lambda o: o.__dict__))
-        review_json = json.loads(json.dumps(Review().__dict__, default=lambda o: o.__dict__))
-        diary_json = json.loads(json.dumps(Diary().__dict__, default=lambda o: o.__dict__))
-        station_json = json.loads(json.dumps(Station().__dict__, default=lambda o: o.__dict__))
-        direction_json = json.loads(json.dumps(Direction().__dict__, default=lambda o: o.__dict__))
-        event_json = json.loads(json.dumps(DailyEvent().__dict__, default=lambda o: o.__dict__))
+        # # # GET MODEL METADATA # # #
+        models_json = ModelMetadata.get_model_metadata()
 
         print('END pub')
         name = "readonly"
         page = "pub"
 
-
         return render_template('03_pub_.html',
                                pub_1=pub_json,
                                events=json_loads,
-                               event=event_json,
-                               photos_list=photos_list,
+                               event=models_json['event_json'],
+                               photos_list=source_data['photos_list'],
                                env_vars=env_vars,
-                               detail=detail_json,
-                               review=review_json,
-                               diary=diary_json,
-                               station=station_json,
-                               direction=direction_json,
-                               directions_list=directions_list,
-                               stations_directions_list=stations_directions_list,
+                               detail=models_json['detail_json'],
+                               review=models_json['review_json'],
+                               diary=models_json['diary_json'],
+                               station=models_json['station_json'],
+                               direction=models_json['direction_json'],
+                               directions_list=source_data['directions_list'],
+                               stations_directions_list=source_data['stations_directions_list'],
                                photo=Photo(),
                                name=name,
                                filters=filters,
                                page=page,)
     else:
-        # return render_template('02_home_.html')
         return redirect(url_for('home'))
